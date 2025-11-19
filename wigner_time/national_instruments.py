f9ab148407fdd77adb8ead9d7f2c6f49d18edd02
ctw <clark.thomaswilliam@zoho.eu>
2025-11-19 18:53:00 +0100

    added warning and started dev notes

--- wigner_time/national_instruments.py
+++ wigner_time/national_instruments.py
@@ -1,3 +1,10 @@
 """
-This is currently not implemented, but if it is of interest to you then please don't hesitate to get in touch.
+This is currently not implemented, but if it is of interest to you then please don't hesitate to get in touch: thomas.clark@wigner.hun-ren.hu .
 """
+import warnings
+
+warnings.warn(
+    "This is currently not implemented, but if it is of interest to you then please don't hesitate to get in touch: thomas.clark@wigner.hun-ren.hu ."
+    UserWarning,
+    stacklevel=2
+)
