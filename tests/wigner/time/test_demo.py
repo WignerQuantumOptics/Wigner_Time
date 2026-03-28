f3014a4b381106ccaab9dab6fcaabcddc6243701
ctw <clark.thomaswilliam@zoho.eu>
2026-03-28 22:46:48 +0100

    bug in test_fullDemo from index 57

--- tests/wigner/time/test_demo.py
+++ tests/wigner/time/test_demo.py
@@ -311,9 +311,10 @@ def test_fullDemo():
         ex.molasses(),
         ex.optical_pumping(),
         ex.magnetic_trapping(),
-        ex.pull_coils(50e-3, -4.1, -4.7, -0.6, -0.6),
-        ex.finish(),
+        # ex.pull_coils(50e-3, -4.1, -4.7, -0.6, -0.6),
+        # ex.finish(),
     ).drop(columns=["function"])
+    # TODO: WIP
     expected = pd.DataFrame(
         {
             "time": [
@@ -754,4 +755,11 @@ def test_fullDemo():
             ],
         }
     )
+
+    print('actual')
+    i1=50
+    i2=i1+10
+    print(actual[i1:i2])
+    print('expected')
+    print(expected[i1:i2])
     return frame.assert_equal(actual, expected)
