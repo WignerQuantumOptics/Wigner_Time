cd4eb0d2cddf16c92c31d52443a017f7cc45ee94
ctw <clark.thomaswilliam@zoho.eu>
2026-03-28 08:57:56 +0100

    merge

--- doc/demo__tips_and_tricks.py
+++ doc/demo__tips_and_tricks.py
@@ -1,3 +1,14 @@
 """
 Here we highlight some convenient features that might not be obvious otherwise.
 """
+
+from wigner_time import timeline as tl
+
+tl.create(AOM_MOT=1, shutter_MOT=1, t=10, context="MOT")
+
+tl.create(AOM_MOT=[0.1, 1], shutter_MOT=[0.0, 1], thing=40, context="MOT")
+
+tl.create(AOM_MOT=[[0.1, 1], [0.2, 0]], context="MOT")
+tl.create("AOM_MOT", 0.1, 1, "MOT")
+
+tl.create([["AOM_MOT", 1, 0.1, "MOT"], ["AOM_imaging", 1, 0.0, "AI"]])
